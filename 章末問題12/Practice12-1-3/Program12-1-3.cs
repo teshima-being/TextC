764786f0920904e945e8a78fb39b365230286a3e
Yusuke Teshima <teshima@beingcorp.co.jp>
2024-05-13 15:26:36 +0900

    #486306[C#テキスト]12章演習問題
    11章の章末問題の解答です。

---
+++ 章末問題12/Practice12-1-3/Program12-1-3.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+using System.Xml;
+using Practice12_1_1;
+
+namespace Practice12_1_3 {
+    internal class Program {
+
+        //問題12-1-3
+        //12-1-2で作成したファイルを読み込み、逆シリアル化してください。
+
+        static void Main(string[] args) {
+
+            using (var wReader = XmlReader.Create(@"..\..\..\Practice12-1-2\employees.xml")) {
+                var wSerializer = new DataContractSerializer(typeof(Employee[]));
+                var wEmployees = wSerializer.ReadObject(wReader) as Employee[];
+
+                //結果確認用
+                foreach (var wEmployee in wEmployees) {
+                    Console.WriteLine(wEmployee.ToString());
+                }
+            }
+        }
+    }
+}
