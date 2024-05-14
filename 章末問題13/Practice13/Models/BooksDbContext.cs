f13a0e4ba173fe959f47326f238f69eca93137cb
Yusuke Teshima <teshima@beingcorp.co.jp>
2024-05-14 14:16:17 +0900

    #486307[C#テキスト]13章演習問題
    13章の章末問題の解答です。

---
+++ 章末問題13/Practice13/Models/BooksDbContext.cs
@@ -0,0 +1,28 @@
+﻿using System.Data.Entity;
+
+namespace Practice13.Models {
+    public class BooksDbContext : DbContext {
+        // コンテキストは、アプリケーションの構成ファイル (App.config または Web.config) から 'BooksDbContext' 
+        // 接続文字列を使用するように構成されています。既定では、この接続文字列は LocalDb インスタンス上
+        // の 'Practice13.Models.BooksDbContext' データベースを対象としています。 
+        // 
+        // 別のデータベースとデータベース プロバイダーまたはそのいずれかを対象とする場合は、
+        // アプリケーション構成ファイルで 'BooksDbContext' 接続文字列を変更してください。
+        public BooksDbContext()
+            : base("name=BooksDbContext") {
+        }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Author> Authors { get; set; }
+
+        // モデルに含めるエンティティ型ごとに DbSet を追加します。Code First モデルの構成および使用の
+        // 詳細については、http://go.microsoft.com/fwlink/?LinkId=390109 を参照してください。
+
+        // public virtual DbSet<MyEntity> MyEntities { get; set; }
+    }
+
+    //public class MyEntity
+    //{
+    //    public int Id { get; set; }
+    //    public string Name { get; set; }
+    //}
+}
---
+++ 章末問題13/Practice13/Program.cs
@@ -0,0 +1,129 @@
+﻿using Practice13.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Practice13 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            //問題13-1
+            //以下の2名の著者と4冊の書籍をデータに追加してください。
+
+            /*
+            《著者》
+             菊池寛　　1888年12月26日生まれ　　男性
+             川端康成　1899年 6月14日生まれ　　男性
+
+            《書籍》
+             こころ　　　　　　1991年出版　　夏目漱石 著
+             伊豆の踊子　　　2003年出版　　川端康成 著
+             真珠婦人　　　　　2002年出版　　菊池寛　 著
+             注文の多い料理店　2000年出版　　宮沢賢治 著
+            */
+
+            var wAuthors = new Author[] {
+                new Author("菊池寛", new DateTime(1888, 12, 26), "M"),
+                new Author("川端康成", new DateTime(1899, 6, 14), "M"),
+            };
+            AddAuthors(wAuthors);
+
+            var wBooks = new Book[] {
+                new Book("こころ", 1991, new Author("夏目漱石", new DateTime(1867, 2, 9), "M")),
+                new Book("伊豆の踊子", 2003, new Author("川端康成", new DateTime(1899, 6, 14), "M")),
+                new Book("真珠夫人", 2002, new Author("菊池寛", new DateTime(1888, 12, 26), "M")),
+                new Book("注文の多い料理店", 2000, new Author("宮沢賢治", new DateTime(1896, 8, 27), "M")),
+            };
+            AddBooks(wBooks);
+
+
+            //問題13-2
+            //すべての書籍情報を著者名とともに表示するコードを書き、上記で追加したデータが正しく追加されたか
+            //確認してください。
+
+            DisplayAllBooks();
+            Console.WriteLine("*****************************************************"); //仕切り
+
+
+            //問題13-3
+            //タイトルが最も長い書籍を求めてください。複数ある場合は、すべてを求めて表示してください。
+
+            using (var wDB = new BooksDbContext()) {
+                Console.WriteLine("タイトルが最も長い書籍は以下となります。");
+                foreach (Book wBook in wDB.Books.Where(x => x.Title.Length == wDB.Books.Max(y => y.Title.Length))) {
+                    Console.WriteLine($"{wBook.Title} ({wBook.Author.Name} 著)");
+                }
+            }
+            Console.WriteLine("*****************************************************"); //仕切り
+
+
+            //問題13-4
+            //出版年の古い順に3冊だけ書籍を取得し、そのタイトルと著者名を求めてください。
+
+            using (var wDB = new BooksDbContext()) {
+                Console.WriteLine("出版年が古い順に3冊だけ抽出した結果は以下の通りです。");
+                foreach (Book wBook in wDB.Books.OrderBy(x => x.PublishedYear).Take(3)) {
+                    Console.WriteLine($"{wBook.Title} ({wBook.Author.Name} 著) {wBook.PublishedYear}年出版");
+                }
+            }
+            Console.WriteLine("*****************************************************"); //仕切り
+
+
+            //問題13-5
+            //著者ごとに書籍のタイトルと出版年を表示してください。なお、著者は誕生日の遅い順に並べてください。
+
+            using (var wDB = new BooksDbContext()) {
+                foreach (IGrouping<Author, Book> wGroup in wDB.Books.GroupBy(x => x.Author).OrderBy(x => x.Key.Birthday)) {
+                    Console.WriteLine($"<{wGroup.Key.Name} ({wGroup.Key.Birthday:d})>");
+
+                    foreach (Book wBook in wGroup) {
+                        Console.WriteLine($"・{wBook.Title} ({wBook.PublishedYear}年出版)");
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// 全ての書籍のデータをコンソールに表示します
+        /// </summary>
+        static void DisplayAllBooks() {
+            using (var wDB = new BooksDbContext()) {
+                foreach (Book wBook in wDB.Books) {
+                    Console.WriteLine($"{wBook.Title}, {wBook.PublishedYear}年出版, {wBook.Author.Name} 著");
+                }
+            }
+        }
+
+        /// <summary>
+        /// 著者クラスのコレクションを引数に取り、データベースに著者データのみ追加します
+        /// </summary>
+        /// <param name="vAuthors">著者クラスのコレクション</param>
+        static void AddAuthors(IEnumerable<Author> vAuthors) {
+            using (var wDB = new BooksDbContext()) {
+                foreach (Author wAuthor in vAuthors) {
+                    wDB.Authors.Add(wAuthor);
+                }
+                wDB.SaveChanges();
+            }
+        }
+
+        /// <summary>
+        /// 書籍クラスのコレクションを引数に取り、データベースに書籍データのみ追加します
+        /// </summary>
+        /// <param name="vBooks">書籍クラスのコレクション</param>
+        static void AddBooks(IEnumerable<Book> vBooks) {
+            using (var wDB = new BooksDbContext()) {
+                foreach (Book wBook in vBooks) {
+                    if (!wDB.Authors.Any(x => x.Name == wBook.Author.Name)) {
+                        Console.WriteLine($"{wBook.Title}は著者情報の登録がありません。著者情報を登録してください。");
+                        continue;
+                    }
+                    var wAuthor = wDB.Authors.Single(x => x.Name == wBook.Author.Name);
+                    wBook.Author = wAuthor;
+                    wDB.Books.Add(wBook);
+                }
+                wDB.SaveChanges();
+            }
+        }
+    }
+}
