fe4752b9b4cf4a49eb04607178f4199500374ff7
Yusuke Teshima <teshima@beingcorp.co.jp>
2024-05-17 16:38:40 +0900

    #486310[C#テキスト]15章演習問題
    15章の章末問題の解答です。

---
+++ 章末問題15/Practice15/Category.cs
@@ -0,0 +1,36 @@
+﻿namespace Practice15 {
+
+    /// <summary>
+    /// カテゴリクラス
+    /// </summary>
+    internal class Category {
+
+        /// <summary>
+        /// カテゴリID
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// カテゴリ名
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// カテゴリクラスのコンストラクタ
+        /// </summary>
+        /// <param name="vId"></param>
+        /// <param name="vName"></param>
+        public Category(int vId, string vName) {
+            this.Id = vId;
+            this.Name = vName;
+        }
+
+        /// <summary>
+        /// 結果の確認を容易にするためにToStringメソッドをオーバーライドしています。
+        /// </summary>
+        /// <returns>カテゴリクラスのプロパティが確認できる文字列</returns>
+        public override string ToString() {
+            return $"Id:{this.Id}, カテゴリ名:{this.Name}";
+        }
+    }
+}
---
+++ 章末問題15/Practice15/Library.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+
+namespace Practice15 {
+
+    /// <summary>
+    /// Libraryクラス
+    /// </summary>
+    internal class Library {
+
+        /// <summary>
+        /// カテゴリ一覧
+        /// </summary>
+        public static IEnumerable<Category> Categories { get; private set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public static IEnumerable<Book> Books { get; private set; }
+
+        /// <summary>
+        /// Libraryクラスのコンストラクタ
+        /// </summary>
+        static Library() {
+            Books = FBooks;
+            Categories = FCategories;
+        }
+
+        //追加する書籍のリスト
+        public readonly static List<Book> FBooks = new List<Book>() {
+                new Book("Writing C# Solid Code", 1, 2500, 2016),
+                new Book("C# 開発指南", 1, 3800, 2014),
+                new Book("Visual C# 再入門", 1, 2780, 2016),
+                new Book("フレーズで学ぶ C# Book", 1, 2400, 2016),
+                new Book("TypeScript 初級講座", 1, 2500, 2015),
+                new Book("PowerShell 実践レシピ", 2, 4200, 2013),
+                new Book("SQL Server 完全入門", 2, 3800, 2014),
+                new Book("IIS Web サーバー運用ガイド", 2, 3180, 2015),
+                new Book("Microsoft Azure サーバー構築", 2, 4800, 2016),
+                new Book("Web デザイン講座 HTML5 & CSS", 3, 2800, 2013),
+                new Book("HTML5 Web 大百科", 3, 3800, 2015),
+                new Book("CSS デザイン 逆引き辞典", 3, 3550, 2015),
+                new Book("Windows10 で楽しくお仕事", 4, 2280, 2016),
+                new Book("Windows10 使いこなし術", 4, 1890, 2015),
+                new Book("続 Windows10 使いこなし術", 4, 2080, 2016),
+                new Book("Windows10 やさしい操作入門", 4, 2300, 2015),
+                new Book("まるわかり Microsoft Office 入門", 5, 1890, 2015),
+                new Book("Word・Excel 実践テンプレート集", 5, 2600, 2016),
+                new Book("楽しく学ぶ Excel 初級編", 5, 2800, 2015),
+            };
+
+        //追加する書籍のカテゴリのリスト
+        public readonly static List<Category> FCategories = new List<Category>() {
+                new Category(1, "Development"),
+                new Category(2, "Server"),
+                new Category(3, "Web Design"),
+                new Category(4, "Windows"),
+                new Category(5, "Application"),
+            };
+    }
+}
